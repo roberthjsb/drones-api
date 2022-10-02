3188ef92e2e719c1e766f6198e9bb9636c0b65f3
roberthjsb <roberth.salaza@gmail.com>
2022-10-02 07:59:54 -0300

    configuración inicial de la api y swagger

---
+++ drones-api/Controllers/DronesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace drones_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DronesController : ControllerBase
+    {
+        // GET: api/<DronesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DronesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DronesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DronesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
